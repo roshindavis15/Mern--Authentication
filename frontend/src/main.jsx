c3588684427c7e5e1b49c42f4345169f3236f256
roshin <roshindavis15@gmail.com>
2024-02-02 20:38:40 +0530

    components created and routes implimented

---
+++ frontend/src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import HomeScreen from './screens/HomeScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+        <Route index={true} path='/' element={<HomeScreen/>}></Route>
+        <Route  path='/login' element={<LoginScreen/>}></Route>
+        <Route  path='/register' element={<RegisterScreen/>}></Route>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
